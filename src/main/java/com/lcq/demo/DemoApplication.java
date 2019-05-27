a4e02e1e09fcf1937bf16153570900ae856597de
linchaoquande <linchaoquande@aliyun.com>
2019-05-27 17:11:09 +0800

    commit2

--- src/main/java/com/lcq/demo/DemoApplication.java
+++ src/main/java/com/lcq/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import de.codecentric.boot.admin.config.EnableAdminServer;
 
 @SpringBootApplication
 @EnableAdminServer
+//
 public class DemoApplication {
 
 	public static void main(String[] args) {
